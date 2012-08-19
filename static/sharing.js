b812e70dc54b9091a341c7d6dbdccf9137dfcefb
Astro <astro@spaceboyz.net>
2012-08-19 04:13:48 +0200

    js: <progress>

--- static/sharing.js
+++ static/sharing.js
@@ -4,13 +4,37 @@ function generateToken() {
 	Math.round(999999 * Math.random());
 }
 
+function fileSize(input) {
+    var total = 0;
+    for(var i = 0; i < input.files.length; i++) {
+	total += input.files[i].size;
+    }
+    return total;
+}
+
+function humanRate(rate) {
+    var unit = "";
+    var units = "KMGT";
+    while(rate > 1024 && units) {
+	rate /= 1024;
+	unit = units.slice(0, 1);
+	units = units.slice(1);
+    }
+    if (rate < 10)
+	rate = Math.round(rate * 10) / 10;
+    else
+	rate = Math.round(rate);
+    return rate + " " + unit + "B/s";
+}
+
 $('form').append("<iframe name='blackhole' style='display: none'></iframe>");
 $('form').attr('target', 'blackhole');
 var token = generateToken();
 $('form').attr('action', $('form').attr('action') + token);
 $('form').submit(function() {
-    var el = $('<p></p>');
-    $('form').after(el);
+    var progress = $('<p><progress></progress><span class="rate"></span></p>');
+    progress.find('progress').prop('max', fileSize($('form input:file')[0]));
+    $('form').after(progress);
     var desc = $("<div class='description'><h2>Add a description</h2><textarea rows='4' cols='40'></textarea><input type='submit' value='Add'></div>");
     desc.find('input').click(function() {
 	$.ajax({ type: 'POST',
@@ -21,7 +45,7 @@ $('form').submit(function() {
 	       });
 	desc.remove();
     });
-    $(el).after(desc);
+    $(progress).after(desc);
     $('form').hide();
 
     /* In case AJAX request comes before form submit, to retry with
@@ -29,21 +53,22 @@ $('form').submit(function() {
     var errorRetries = 10;
 
     function pollProgress() {
-	console.log("poll");
 	$.ajax({ url: "/progress/" + token,
 		 dataType: 'json',
 		 success: function(data) {
 		     errorRetries = 0;
 
 		     if (data.bytes && data.rate) {
-			 el.text(data.bytes + " " + data.rate);
+			 progress.find('progress').prop('value', data.bytes);
+			 progress.find('.rate').text(humanRate(data.rate));
 			 pollProgress();
 		     } else if (data.link) {
-			 el.text("Upload finished: ");
+			 progress.empty();
+			 progress.text("Upload finished: ");
 			 var link = $('<a></a>');
 			 link.attr('href', data.link);
 			 link.text(data.link);
-			 el.append(link);
+			 progress.append(link);
 
 			 setTimeout(pollProgress, 10 * 1000);
 		     }
